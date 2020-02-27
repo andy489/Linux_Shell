e7bd459edb884436bea3064583a08a6037d3282e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-27 11:43:20 +0200

    Create Task 002 Solution.sh

---
+++ Tasks/Task 002 Solution.sh
@@ -0,0 +1,25 @@
+for num in {1..99..2}
+do
+    echo $num
+done
+
+OR
+
+seq 1 2 99
+
+OR
+
+for ((i=1; i<100; i=i+2))
+do
+    echo $i
+done
+
+OR
+
+num=1
+while [ $num -le 99 ]
+do
+
+    echo $num
+    num=$((num+2))
+done
