ebe693cd344a58ce7ab236af89d9536843a81350
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-27 15:43:47 +0200

    Create Task 004 Solution.sh

---
+++ Tasks/Task 004 Solution.sh
@@ -0,0 +1,24 @@
+for num in {1..50}
+do
+    echo $num
+done
+
+OR
+
+seq 1 50
+
+OR
+
+for ((i=1; i<=50; i=i+1))
+do
+    echo $i
+done
+
+OR
+
+num=1
+while [ $num -le 50 ]
+do
+    echo $num
+    num=$((num+1))
+done
