afee3e1e8f301787a00b0f1e001931ee55623bdf
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-01 22:32:34 +0300

    Create Draw Pyramid.sh

---
+++ FMI Tasks/Bash Scripts/Draw Pyramid.sh
@@ -0,0 +1,22 @@
+# github.com/andy489
+
+!#/bin/bash
+
+#!/bin/bash
+
+n=$1 
+  
+for((r=1; r<=n; ++r)) 
+do
+    for((j=0; j<n-r; ++j))
+    do	 
+      	echo -n " "
+    done
+    	
+    for((k=0; k<r; ++k))
+    do
+        echo -n "*"
+    done 
+
+    echo	
+done
