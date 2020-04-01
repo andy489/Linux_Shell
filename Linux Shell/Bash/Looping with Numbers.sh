5c19a484393e1d43a79d9a3670843278f33cbcdf
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-01 20:06:11 +0300

    Create Looping with Numbers.sh

---
+++ Linux Shell/Bash/Looping with Numbers.sh
@@ -0,0 +1,26 @@
+# github.com/andy489
+
+for num in {1..50}
+do
+    echo $num
+done
+
+# ---- OR
+
+seq 1 50
+
+OR
+
+for ((i=1; i<=50; i=i+1))
+do
+    echo $i
+done
+
+# ---- OR
+
+num=1
+while [ $num -le 50 ]
+do
+    echo $num
+    num=$((num+1))
+done
