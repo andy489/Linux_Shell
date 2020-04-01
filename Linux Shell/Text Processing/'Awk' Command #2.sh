3d8886b73ad9c4bc119ccc6fe0d115e8d49d1842
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-01 22:13:31 +0300

    Create 'Awk' Command #2.sh

---
+++ Linux Shell/Text Processing/'Awk' Command #2.sh
@@ -0,0 +1,12 @@
+# github.com/andy489
+
+awk '{
+if($2 < 50 || $3 < 50 || $4 < 50) 
+    print $1,":","Fail";
+else 
+    print $1,":","Pass";
+}'
+
+# ---- OR
+
+awk '{print $1,":",($2<50||$3<50||$4<50) ? "Fail" : "Pass"}'
