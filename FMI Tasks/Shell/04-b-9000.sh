e694d130e501ab29b50e1e531163fe146155f7a6
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-03 23:19:55 +0300

    Create 04-b-9000.sh

---
+++ FMI Tasks/Shell/04-b-9000.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 04-b-9000
+# github.com/andy489
+
+function count_children {
+        ps --ppid "${1}" | head +2 | wc -l
+}
+
+ps -eo pid=,ppid= | while read  -d $'\n' pid_and_ppid; do
+        read pid ppid < <(echo "${pid_and_ppid}")
+        [[ $(count_children "${pid}") -ge $(count_children "${ppid}") ]] && echo "${pid}"
+done 2>/dev/null
