0eb75c1dcff09473bb37d8ddab787d05bbcd019a
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-11 15:14:05 +0300

    Create 05-b-4200 2nd sol.sh

---
+++ FMI Tasks/Shell/05-b-4200 2nd sol.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 05-b-4200 2nd solution
+# github.com/andy489
+
+[ $# -eq 1 ] || exit 1
+[ ! -r $1 ] || exit 2
+
+F=${1}
+
+DATA=$(cat $F | sed -e 's/[^{}]//g' | tr -d '\n' | tr -d ' ')
+while grep -qsF '{}{' <(echo ${DATA}); do
+	DATA=$(echo "${DATA}" | sed -e 's/{}{/{/')
+done
+
+ALLCHARS=$(echo ${DATA} | tr -d '}' | wc -c)
+echo "The deepest nesting is $((--ALLCHARS)) levels"
