0a4a9454594d3cbaa0b9c67bab40018a6406d635
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-11 16:17:08 +0300

    Create 05-b-7550 1st sol.sh

---
+++ FMI Tasks/Shell/05-b-7550 1st sol.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 05-b-7550 1st solution
+# github.com/andy489
+
+if [ $# -ne 1 ]; then
+	echo "Invalid number of arguments!"
+	exit 1
+fi
+
+USER="${1}"
+
+if [ $(id -u "${USER}") -eq 1 ]; then
+	echo "Invalid username!"
+	exit 2
+fi
+ 
+PS_CNT=0
+
+while read -d $'\n' PID; do
+	kill "${PID}"
+	CNT=(($CNT+1))
+done < <(ps -u "${USER}" -o pid=)
+
+echo "Total: ${PS_CNT} killed processes"
