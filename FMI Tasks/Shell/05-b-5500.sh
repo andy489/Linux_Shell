7ee1c0140d6e3e83186759b80ec03044586bbfdb
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-11 15:59:50 +0300

    Update 05-b-5500.sh

--- FMI Tasks/Shell/05-b-5500.sh
+++ FMI Tasks/Shell/05-b-5500.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # 05-b-5500
+# github.com/andy489
 
-cat ~velin/passwd.txt |\
-        awk -F : 'BEGIN { printf("<table>\n <tr>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n </tr>\n", "Username", "Group", "Login Shell", "GECKO") } { printf(" <tr>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n </tr>\n",$1, $4, $5, $7) } END { printf("</table>\n") }'
+cat ~andreystoev/passwd.txt |\
+        awk -F : 'BEGIN { printf("<table>\n <tr>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n </tr>\n",
+        "Username", "Group", "Login Shell", "GECKO") } 
+        { printf(" <tr>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n </tr>\n",$1, $4, $5, $7) } 
+        END { printf("</table>\n") }'
