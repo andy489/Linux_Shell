6990991a15720306ea5830ef8fb54929518ba2ab
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-11 15:56:47 +0300

    Create 05-b-4800.sh

---
+++ FMI Tasks/Shell/05-b-4800.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# 05-b-4800.sh
+# github.com/andy489
+
+if [ $# -ne 2 ]; then
+	echo "Invalid number of arguments!"
+	exit 1
+fi
+
+FILE_PATH="${1}"
+DIR_PATH="${2}"
+
+if [ ! -f "${FILE_PATH}" ]; then
+	echo "First argument is not a file!"
+	exit 2
+elif [ ! -r "${FILE_PATH}" ]; then
+	echo "File is not readable!"
+	exit 3
+fi
+
+if [ ! -d "${DIR_PATH}" ]; then
+	echo "Second argument is not a directory!"
+	exit 4
+elif [ ! -r "${DIR_PATH}" ]; then
+	echo "Directory is not readable!"
+	exit 5
+fi
+
+FILE_HASH="$(md5sum "${FILE_PATH}" | cut -d' ' -f1)"
+CNT=0
+while read -d $'\0' FILE; do
+	CUR_HASH="$(md5sum "${FILE}" | cut -d' ' -f1)"
+	if [ "${CUR_HASH}" = "${FILE_HASH}" ]; then
+		CNT=$(($CNT+1))
+		echo $(basename "${FILE}")
+	fi
+done < <(find "${DIR_PATH}" -type f 2>/dev/null -print0)
+
+if [ $CNT -eq 0 ]; then
+	echo "No copies found."
+else
+	echo "~Found total $CNT duplicates."
+fi
