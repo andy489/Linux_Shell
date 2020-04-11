2328536625c790606c7c3e71931d5f065b8302ed
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-11 15:59:09 +0300

    Create 05-b-5500.sh

---
+++ FMI Tasks/Shell/05-b-5500.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 05-b-5500
+
+cat ~velin/passwd.txt |\
+        awk -F : 'BEGIN { printf("<table>\n <tr>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n  <th>%s</th>\n </tr>\n", "Username", "Group", "Login Shell", "GECKO") } { printf(" <tr>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n  <td>%s</td>\n </tr>\n",$1, $4, $5, $7) } END { printf("</table>\n") }'
