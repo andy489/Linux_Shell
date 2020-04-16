89b3e4958cb7035790f4f27b5a8a2fd1ca31b14f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-16 13:38:03 +0300

    Create 05-b-4301.sh

---
+++ FMI Tasks/Shell/05-b-4301.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 05-b-4301
+# github.com/andy489
+
+if [ $# -ne 3 ]; then
+	echo "Invalid number of arguments!"
+	exit 1
+fi
+
+ADDRESS_FILE="${1}"
+
+if [ ! -f "${ADDRESS_FILE}" ]; then
+	echo "First argument is not a file!"
+	exit 2
+elif [ ! -r "${ADDRESS_FILE}" ]; then
+	echo "Address book file is not readable!"
+	exit 3
+elif [ ! -w "${ADDRESS_FILE}" ]; then
+	echo "Address book file is not writable!"
+	exit 4
+fi
+
+NAME="${2}"
+NICK="${3}"
+
+USERNAME=$(cat ~velin/passwd.txt | awk -v name="${NAME}" -F ':' '($5 == name){print $1}')
+
+#if [ $(wc -l "${USERNAME}") -ne 1 ]; then
+#	echo "Do the bonus thing"
+#fi
+
+echo "${NICK} ${USERNAME}" >> "${ADDRESS_FILE}"
