9dc60e1e8532a9bb28970b00f1ac837280e2a3c9
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-23 17:48:31 +0300

    Update 13 (solution 2).sh

--- FMI Tasks/Test 2 Preparation/13 (solution 2).sh
+++ FMI Tasks/Test 2 Preparation/13 (solution 2).sh
@@ -2,11 +2,11 @@
 # 13.sh (solution 2)
 # github.com/andy489
 
-[ $# -eq 1 ] || (echo "Invalid number of arguments"; exit 1)
+[ $# -eq 1 ] || { echo "Invalid number of arguments"; exit 1; }
 
 d="${1}"
 
-[ -d "${d}" -a -r "${d}" ] || (echo "Not directory or not readable"; exit 2)
+[ -d "${d}" -a -r "${d}" ] || { echo "Not directory or not readable"; exit 2; }
 
 find "${di}" -type l 2>/dev/null -print0 \
 	| xargs -0 -I {} file {} \
