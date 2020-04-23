4458d4879490017be16eef7574febf0c5eb7ed96
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-23 20:07:50 +0300

    Update 05-b-9600.sh

--- FMI Tasks/Shell/05-b-9600.sh
+++ FMI Tasks/Shell/05-b-9600.sh
@@ -18,9 +18,9 @@ function timestamp {
 
 function archive_and_save {
 	SRC=$1
-	PREFIX=$2
+	POSTFIX=$2
 	TIMESTAMP=$(timestamp)
-	tar -czf "${BACKUP_DIR}/${PREFIX}_${TIMESTAMP}.tgz" "${SRC}"
+	tar -czf "${BACKUP_DIR}/${TIMESTAMP}_${POSTFIX}.tgz" "${SRC}"
 }
 
 function compress_and_save {
@@ -33,10 +33,10 @@ for i; do
 	if [ -d "${i}" ]; then 
 		DIR_CONTENT=$(find "${i}" -mindepth 1 -maxdepth 1 2>/dev/null | wc -l)
 		if [ "${DIR_CONTENT}" -eq 0 ]; then 
-			archive_and_save "${i}" "empty-dir_"
+			archive_and_save "${i}" "empty_dir"
 			rm -i -d "${i}"
 		elif [ "${FLAG}" -eq 1 ];then
-			archive_and_save "${i}" "full-dir_"
+			archive_and_save "${i}" "full_dir"
 			rm -i -d -R "${i}"
 		fi
 	elif [ -f "${i}" ]; then 
