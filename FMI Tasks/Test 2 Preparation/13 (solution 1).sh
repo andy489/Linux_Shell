1de5535c5e3945b3c3aa945bf5a8f1aa0c0a21c0
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-23 16:41:39 +0300

    Create 13 (solution 1).sh

---
+++ FMI Tasks/Test 2 Preparation/13 (solution 1).sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 13.sh solution 1
+# github.com/andy489
+  
+[ $# -eq 1 ] || (echo "Invalid number of arguments"; exit 1)
+
+d="${1}"
+
+[ -d "${d}" -a -r "${d}" ] || (echo "Not directory or not readable"; exit 2)
+
+find -L "${d}" -type l 2>/dev/null
