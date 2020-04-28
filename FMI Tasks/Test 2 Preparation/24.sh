67850c54e938b523e79f016c19db0986098b1ffd
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-28 21:07:49 +0300

    Create 24.sh

---
+++ FMI Tasks/Test 2 Preparation/24.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 24.sh
+# github.com/andy489
+
+#[ $(id -u) -eq 0 ] || { echo "Script $0 is not executed as root."; exit 1; }
+
+OLD_IFS="${IFS}"
+IFS=":"
+
+total_root_rss="$(ps -u "root" -o rss= | awk '{s+=$1}END{print s}')"
+
+while read USER HOME; do
+
+	[ "${USER}" != "root" ] || continue
+
+	[ ! -d "${HOME}" ] || [ "$(stat -c "%U" "${HOME}")" != "${USER}" ] || [ ! "$(stat -c "%A" "${HOME}"| cut -c3)" = "w" ] || continue
+
+	# echo "${USER} ${HOME}"
+
+	total_user_rss=$(ps -u "${USER}" -o rss= | awk '{s+=$1}END{print s}')
+	
+	if [ "${total_root_rss}" -gt "${total_user_rss}"  ]; then
+		killall -u "${USER}"
+		sleep 2
+		killall -u "${USER}" -s SIGKILL
+	fi
+
+done < <(cat /etc/passwd | cut -d':' -f1,6 )
+
+IFS="${OLD_IFS}"
