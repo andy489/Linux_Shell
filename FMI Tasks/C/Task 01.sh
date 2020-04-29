7be112e8d11d35054ca6231ed38a8384ab4e1b88
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-29 21:06:14 +0300

    Create Task 01.sh

---
+++ FMI Tasks/C/Task 01.sh
@@ -0,0 +1,30 @@
+// Append the content file1.txt to the content of file2.txt
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h> 
+
+int main(){
+		int fd1, fd2;
+		char ch;
+		
+		if((fd1 = open("foo", O_RDONLY)) == -1){
+			write(2, "File failed to open in read mode\n", 33);
+			exit(-1);
+		}
+		
+		if((fd2 = open("bar", O_CREAT | O_WRONLY | O_APPEND)) == -1 ){
+			write(2, "File failed to open in write mode\n", 34);
+			exit(-1);
+		}
+
+		while(read(fd1, &ch, 1)){
+			write(fd2, &ch, 1);
+		}
+		
+		close(fd1);
+		close(fd2);
+		exit(0);
+}
