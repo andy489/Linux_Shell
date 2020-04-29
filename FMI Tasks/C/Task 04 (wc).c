cfed8d9036820041203132a95d757f346094d97e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-29 23:39:18 +0300

    Update Task 04 (wc).c

--- FMI Tasks/C/Task 04 (wc).c
+++ FMI Tasks/C/Task 04 (wc).c
@@ -6,41 +6,41 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]){
-    int fd1,
-        lines = 0,
-        words = 0,
-        chars = 0;
-    char c;
+	int fd1,
+		lines = 0, 
+		words = 0, 
+		chars = 0;
+	char c;
+	
+	if (argc != 2) {
+		fprintf(stderr, "err\n");
+		exit(1);
+	}
 
-    if (argc != 2) {
-        fprintf(stderr, "err\n");
-        exit(1);
-    }
+	if ( (fd1 = open(argv[1], O_RDONLY)) == -1 ) {
+		fprintf(stderr, "Operation open failed\n");
+		exit(1);
+	}
 
-    if ( (fd1 = open(argv[1], O_RDONLY)) == -1 ) {
-        fprintf(stderr, "Operation open failed\n");
-        exit(1);
-    }
+	int inWord = 0;
+	while ( read(fd1, &c, 1) > 0 ) {
+		if (c=='\n') {
+			++lines;
+			inWord = 0;
+		}
 
-    int inWord = 0;
-    while ( read(fd1, &c, 1) > 0 ) {
-        if (c=='\n') {
-            ++lines;
-            inWord = 0;
-        }
+		if (c!=' ' && c!='\n' && c!='\t' && c!='\n') {
+			if(!inWord){
+				++words;
+				inWord = 1;
+			}
+		} 
+		else inWord = 0;
+	
+		++chars;
+	}
 
-        if (c!=' ' && c!='\n' && c!='\t' && c!='\n') {
-            if(inWord == 0){
-                ++words;
-                inWord = 1;
-            }
-        } else {
-            inWord = 0;
-        }
-        ++chars;
-    }
-
-    printf("File %s has:\n%d number of lines.\n%d number of words.\n%d number of chars.\n", argv[1], lines, words, chars);
-    close(fd1);
-    exit(0);
+	printf("File %s has:\n%d number of lines.\n%d number of words.\n%d number of chars.\n", argv[1], lines, words, chars);
+	close(fd1);
+	exit(0);
 }
