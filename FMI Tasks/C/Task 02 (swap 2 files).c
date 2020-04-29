1ef998b7f32df9ed8a400b3202abff0935448c52
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-29 23:17:24 +0300

    Update Task 02 (swap 2 files).c

--- FMI Tasks/C/Task 02 (swap 2 files).c
+++ FMI Tasks/C/Task 02 (swap 2 files).c
@@ -2,10 +2,18 @@
 // github.com/andy489
 
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
-int main(int argc, char** argv){
-	exit(renameat2(AT_FDCWD,argv[1],AT_FDCWD,argv[2], RENAME_EXCHANGE));
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 3) {
+        fputs("Wrong number of arguments\n", stderr);
+        return 1;
+    }
+    if(renameat2(AT_FDCWD, argv[1], AT_FDCWD, argv[2], RENAME_EXCHANGE)) {
+        perror("renameat2");
+        return 1;
+    }
+    return 0;
 }
