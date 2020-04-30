e79960aa5ebe6d38a01b1e872cc2d28825476249
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-30 08:02:17 +0300

    Update and rename Task 01 (append) errx err handle.c to Task 01 (append) err error handle.c

--- FMI Tasks/C/Task 01 (append) err error handle.c
+++ FMI Tasks/C/Task 01 (append) err error handle.c
@@ -10,18 +10,18 @@
 
 int main(int argc, char** argv){
         if(argc < 3 ){
-                errx(1, "Invalid number of arguments");
+                err(1, "Invalid number of arguments");
         }
 
         int fd1, fd2;
         char ch;
 
         if((fd1 = open(argv[1], O_RDONLY)) == -1){
-        	errx(2, "File failed to open in read mode");
+        	err(2, "File failed to open in read mode");
 		}
 
         if((fd2 = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, S_IRWXU)) == -1 ){
-                errx(3, "File failed to open in write mode");
+                err(3, "File failed to open in write mode");
         }
 
         while(read(fd1, &ch, 1)){
