b680ae9a3c66736122e43da47e21b50a2a25ccc5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-04-30 07:54:20 +0300

    Update and rename Task 01 (append) adv.cpp to Task 01 (append) adv.c

--- FMI Tasks/C/Task 01 (append) adv.c
+++ FMI Tasks/C/Task 01 (append) adv.c
@@ -1,33 +1,34 @@
 // Append the content file1.txt to the content of file2.txt
 // github.com/andy489
 
+// Append the content file1.txt to the content of file2.txt
+// github.com/andy489
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <err.h>
 
 int main(int argc, char** argv){
         if(argc < 3 ){
-            write(2, "Invalid number of arguments\n", 29);
-            exit(-1);
+                errx(1, "Invalid number of arguments");
         }
 
         int fd1, fd2;
         char ch;
 
         if((fd1 = open(argv[1], O_RDONLY)) == -1){
-            write(2, "File failed to open in read mode\n", 33);
-            exit(-1);
-        }
+        	errx(2, "File failed to open in read mode");
+		}
 
         if((fd2 = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, S_IRWXU)) == -1 ){
-            write(2, "File failed to open in write mode\n", 34);
-            exit(-1);
+                errx(3, "File failed to open in write mode");
         }
 
         while(read(fd1, &ch, 1)){
-            write(fd2, &ch, 1);
+                write(fd2, &ch, 1);
         }
 
         close(fd1);
