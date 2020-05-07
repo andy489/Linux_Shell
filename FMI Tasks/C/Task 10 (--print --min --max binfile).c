0732be8439c8bdc2534bd8b8fe6b56a344135373
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-05-07 15:43:15 +0300

    Update Task 10 (--print --min --max binfile).c

--- FMI Tasks/C/Task 10 (--print --min --max binfile).c
+++ FMI Tasks/C/Task 10 (--print --min --max binfile).c
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
 	if(argc != 3){
 		errx(1,"invalid args cnt, usage: %s [--print | --min | --max] somefile.bin", argv[0]);
 	}
-
+	
 	const char* arg = argv[1];
 
 	if(!(strcmp(arg,"--print")==0||strcmp(arg,"--min")==0||strcmp(arg,"--max")==0)){
@@ -24,48 +24,46 @@ int main(int argc, char** argv){
 	
 	const char* binfile = argv[2];
 	
-	int fd = open(binfile, O_RDONLY);
-	if(fd == -1){
-		err(3,"error while opening %s", binfile);
-	}
-	
 	struct stat st;
 	if(stat(binfile, &st) == -1){
-		int _errno=errno;
-		close(fd);
-		errno=_errno;
-		err(4,"error while stat %s", binfile);
+		err(3,"error while stat %s", binfile);
 	}	
 
 	if(st.st_size == 0){
 		printf("%s is empty\n", binfile);
-		close(fd);
 		exit(0);
 	}
 	
 	if(st.st_size % sizeof(uint16_t) != 0){
-		close(fd);
 		printf("%s does not contain only uint16_t numbers\n", binfile);
-		exit(5);
+		exit(4);
+	}
+
+	if(access(binfile, O_RDONLY) == -1){
+		err(5,"error while opening %s", binfile);
 	}
 
 	uint16_t* nums = malloc(st.st_size);
 	
 	if(nums == NULL){
-		int _errno=errno;
-		close(fd);
-		errno=_errno;
 		err(6,"not enough memory");
 	}
 
 	uint16_t buf;
 	int read_sz;
 	int i=0;
+
+	int fd = open(binfile, O_RDONLY);
+	if(fd == -1){
+		int _errno=errno;
+		free(nums);
+		errno=_errno;
+		err(7,"error while opening %s", binfile); //double check
+	}
 	
 	while((read_sz=read(fd, &buf, sizeof(uint16_t))) > 0){
 		if(read_sz!=sizeof(uint16_t)){
 			int _errno=errno;
-			close(fd);
 			free(nums);
 			errno=_errno;
 			err(7,"error while reading %s", binfile);
@@ -94,8 +92,6 @@ int main(int argc, char** argv){
 		}
 		else printf("%x\n", max);
 	}	
-
-	close(fd);
 	free(nums);
 	exit(0);
 }
