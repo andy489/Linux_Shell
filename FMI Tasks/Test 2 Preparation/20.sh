bcd0cb4340ecca88ec3c35d8fad3cc51d95dbe3c
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-05-09 14:47:51 +0300

    Update 20.sh

--- FMI Tasks/Test 2 Preparation/20.sh
+++ FMI Tasks/Test 2 Preparation/20.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 20.sh
 # github.com/andy489
-  
+
 [ $# -eq 3 ] || { echo "Usage: $0 <dirname> <dirname> <string>"; exit 1; }
 
 src="${1}"
@@ -10,17 +10,19 @@ str="${3}"
 
 [ -d "${src}" ] || { echo "Source is not a directory!"; exit 2; }
 [ -d "${dst}" ] || { echo "Destination is not a directory!"; exit 3; }
-[ -r "${src}" ] || { echo "Source is not readable!"; exit 4; }
-[ -w "${dst}" ] || { echo "Destination is not writable!"; exit 5; }
+[ -r "${src}" -a -w "${src}" ] || { echo "Source does not have \"r\" or \"w\" perm!"; exit 4; }
+[ -r "${dst}" -a -w "${dst}" ] || { echo "Destination does not have \"r\" or \"w\" perm!"; exit 5; }
 
 #[ $(id -u) -eq 0 ] || { echo "Script not run as root (do nothing)!"; exit 0; }
 
-dir_name=$( dirname "${0}" )
-
 dst_content=$( find "${dst}" -type f ! -name ".*" 2>/dev/null | wc -l)
-#[ "${dst_content}" -eq 0 ] || { echo "Destinarion dir must not have other files!"; exit 6; }
+[ "${dst_content}" -eq 0 ] || { echo "Destinarion dir must not have other files!"; exit 6; }
 
-while read f; do
-        base_name=$( basename "${f}" )
-        mv -iv -- "${f}" "${dir_name}/${dst}/${base_name}"
+dir_name="$(dirname "${0}")"
+while read file; do
+   real_dir_base_name="$(echo "${file}" | sed -E "s/${src}\///")"
+
+   mkdir -p "${dst}/$(dirname "${real_dir_base_name}")"
+   # we use cp instead of mv for better testing
+   cp -v -- "${file}" "${dst}/${real_dir_base_name}"
 done < <(find "${src}" -type f -name "*${str}*" 2>/dev/null)
