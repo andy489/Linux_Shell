3c54532a2beaef06164511f62e39ee7b8c7c8dda
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-05-11 19:23:06 +0300

    Update 15.sh

--- FMI Tasks/Test 2 Preparation/15.sh
+++ FMI Tasks/Test 2 Preparation/15.sh
@@ -4,8 +4,8 @@
 
 [ $(id -u) -eq 0 ] || exit 0
 
-while read _USER HOME; do
-        if [ ! -d "${HOME}" ] || sudo -u "${_USER}" [ ! -w "${HOME}" ]; then
+while read _USER _HOME; do
+        if [ ! -d "${_HOME}" ] || sudo -u "${_USER}" [ ! -w "${_HOME}" ]; then
                 echo "${_USER}"
         fi
 done < <(cat /etc/passwd | awk -F : '{print $1,$6}')
