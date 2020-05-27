3e0881e4c4a026ff7b327e5a1c5c6e7c43de40cf
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-05-27 21:18:47 +0300

    Create Task 02.c

---
+++ FMI Tasks/C Processes/Task 02.c
@@ -0,0 +1,22 @@
+/*
+	Да се напише програма на C, която изпълнява команда ls с точно един аргумент.
+*/
+
+#include <stdlib.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		errx(1,"Invalid number of arguments. Usage: %s <filename>", argv[0]);
+	}
+
+	const char* filename = argv[1];	
+
+	if(execlp("ls", "ls", filename, 0) == -1){
+		err(2,"could not execlp ls");
+	}
+	
+	exit(0);
+}
