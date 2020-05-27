52670fdd190e99db48a1e2c0ee594083b13b9192
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-05-27 22:13:29 +0300

    Create Task 04.c

---
+++ FMI Tasks/C Processes/Task 04.c
@@ -0,0 +1,21 @@
+/*
+	Да се напише програма на C, която създава процес дете и демонстрира принцина на конкурентност при процесите.
+*/
+
+#include <stdlib.h>
+#include<stdio.h>
+#include <unistd.h>
+#define N 100000
+
+int main(){
+	if(fork() > 0){
+		for(int i=0; i<N; ++i){
+			printf("parent\n");
+		}
+	} else {
+		for(int i=0; i<N; ++i){
+			printf("child\n");
+		}
+	}
+	exit(0);
+}
