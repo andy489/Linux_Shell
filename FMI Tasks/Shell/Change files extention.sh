4969ce72c3e4ea2a5dae4303434b3e6a05b2d0a5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-10 21:01:21 +0300

    Update and rename Change files extention.md to Change files extention.sh

---
+++ FMI Tasks/Shell/Change files extention.sh
@@ -0,0 +1,16 @@
+// github.com/andy489
+/*
+     Да се напише shell скрипт, който приема точно три аргумента - име на директория, старо файлове разширение и ново файлово
+    разширение. Във всички директории с подаденото име, скрипта да поставя на всички файлове с подаденото старо разширение - новото
+    разширение.
+    
+    Примерно извикване:
+    (change file extension)
+    ./chfext "dir" "avi" "mp3"
+    
+    Резултат:
+    renamed '/home/students/s61000/dir/film.avi' -> '/home/students/s61000/dir/film.mp3'
+    renamed '/home/students/s61000/dir/mov1.avi' -> '/home/students/s61000/dir/mov1.mp3'
+    renamed '/home/students/s61000/dir/mov2.avi' -> '/home/students/s61000/dir/mov2.mp3'
+    renamed '/home/students/s61000/folder/dir/deeper.avi' -> '/home/students/s61000/folder/dir/deeper.mp3'
+*/
