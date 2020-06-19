83cf27a1285838a2935c075e6fe2e2fdc38a8897
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-20 00:40:35 +0300

    Update 24.sh

--- FMI Tasks/Test 2 Preparation/24.sh
+++ FMI Tasks/Test 2 Preparation/24.sh
@@ -10,20 +10,20 @@ TOTAL_ROOT_RSS="$(ps -u "root" -o rss= | awk '{s+=$1}END{print s}')"
 
 while read _USER _HOME; do
 
-	[ "${_USER}" != "root" ] || continue
+	[ "${USER_UID}" != "root" ] || continue
 	
-	[ ! -d "${_HOME}" ] || [ "$(stat -c "%U" "${_HOME}")" != "${_USER}" ] || [ "$(stat -c "%A" "${_HOME}"| cut -c3)" != "w" ] || continue
+	[ ! -d "${_HOME}" ] || [ "$(stat -c "%u" "${_HOME}")" != "${USER_UID}" ] || [ "$(stat -c "%A" "${_HOME}"| cut -c3)" != "w" ] || continue
 
 	# echo "${_USER} ${_HOME}"
 
-	TOTAL_USER_RSS="$(ps -u "${_USER}" -o rss= | awk '{s+=$1}END{print s}')"
+	TOTAL_USER_RSS="$(ps -u "${USER_UID}" -o rss= | awk '{s+=$1}END{print s}')"
 	
 	[ -n "${TOTAL_USER_RSS}" ] || TOTAL_USER_RSS=0
 	
 	if [ "${TOTAL_ROOT_RSS}" -gt "${TOTAL_USER_RSS}" ]; then
-		killall -u "${_USER}" -m .
+		killall -u "${USER_UID}" -m .
 		sleep 2
-		killall -u "${_USER}" -KILL -m .
+		killall -u "${USER_UID}" -KILL -m .
 	fi
 
-done < <(cut -d':' -f1,6 /etc/passwd)
+done < <(cut -d':' -f3,6 /etc/passwd)
