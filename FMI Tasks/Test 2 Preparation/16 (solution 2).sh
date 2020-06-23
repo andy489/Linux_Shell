ee7af6f3693ebb150f0e1399ed62312c584b7ca5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-23 23:55:07 +0300

    Update 16 (solution 2).sh

--- FMI Tasks/Test 2 Preparation/16 (solution 2).sh
+++ FMI Tasks/Test 2 Preparation/16 (solution 2).sh
@@ -2,27 +2,48 @@
 # 16.sh
 # github.com/andy489
 
-[ $# -eq 3 ] || { echo "Invalid number of arguments"; exit 1; }
+#!/bin/bash
 
-FILE="${1}"; KEY_1="${2}"; KEY_2="${3}"
+[ $# -eq 3 ] || { 
+echo "Invalid number of arguments. Usage: $0 <filename> <str1> <str2>" 
+exit 1
+}
 
-[ -f "${FILE}" -a -r "${FILE}" ] || { echo "Not file ot not readable"; exit 2; }
-[ -n "${KEY_1}" -a -n "${KEY_2}" ] || { echo "There is a key with length 0"; exit 3; }
+FILE_PATH="${1}"
 
-VAL_1="$(cat "${FILE}" | fgrep "${KEY_1}=" | awk -F'=' '{print $2}')"
-VAL_2="$(cat "${FILE}" | fgrep "${KEY_2}=" | awk -F'=' '{print $2}')"
+[ -f "${FILE_PATH}" ] || { echo "File ${FILE_PATH}" does not exist; exit 2; }
+
+[ -r "${FILE_PATH}" ] || { echo "File ${FILE_PATH}" is not readable; exit 3; }
+
+[ -w "${FILE_PATH}" ] || { echo "File ${FILE_PATH}" is not writable; exit 4; }
+
+KEY_1="${2}"
+KEY_2="${3}"
+
+[ -n "${KEY_1}" -a -n "${KEY_2}" ] || { echo "There is a key with zero length"; exit 5; }
+
+VAL_1=$(fgrep "${KEY_1}=" "${FILE_PATH}" | sed "s/${KEY_1}=//")
+VAL_2=$(fgrep "${KEY_2}=" "${FILE_PATH}" | sed "s/${KEY_2}=//")
+
+# alternative with awk:
+# VAL_1="$(fgrep "${KEY_1}=" "${FILE_PATH}" | awk -F'=' '{print $2}')"
+# VAL_2="$(fgrep "${KEY_2}=" "${FILE_PATH}" | awk -F'=' '{print $2}')"
+
+# echo "${VAL_1}"
+# echo "${VAL_2}"
 
 [ -n "${VAL_2}" ] || exit 0
 
 NEW_VAL=""
+
 for WORD in ${VAL_2}; do
 	if fgrep -qv "${WORD}" <(echo "${VAL_1}"); then
 		NEW_VAL+="${WORD} "
 	fi
 done
 
-NEW_VAL="$(echo "${NEW_VAL%?}" )"
+NEV_VAL="$(echo "${NEW_VAL%?}" )" # removing the last space symbol
 
-sed -i -E "s/${KEY_2}=${VAL_2}/${KEY_2}=${NEW_VAL}/" "${FILE}"
+sed -i -E "s/${KEY_2}=${VAL_2}/${KEY_2}=${NEW_VAL}/" "${FILE_PATH}"
 
-exit #?	
+exit $?
