1500952ce05256d2fa9422bed60562f1bfd08e28
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-24 22:41:24 +0300

    Update 04-b-9000 1st sol.sh

--- FMI Tasks/Shell/04-b-9000 1st sol.sh
+++ FMI Tasks/Shell/04-b-9000 1st sol.sh
@@ -2,11 +2,10 @@
 # 04-b-9000 1st solution
 # github.com/andy489
 
-function count_children {
+function count_children() {
         ps --ppid "${1}" | head +2 | wc -l
 }
 
-ps -eo pid=,ppid= | while read  -d $'\n' pid_and_ppid; do
-        read pid ppid < <(echo "${pid_and_ppid}")
-        [ $(count_children "${pid}") -ge $(count_children "${ppid}") ] && echo "${pid}"
+ps -eo pid=,ppid= | while read PID PPID; do
+        [ $(count_children "${PID}") -ge $(count_children "${PPID}") ] && echo "${PID}"
 done 2>/dev/null
