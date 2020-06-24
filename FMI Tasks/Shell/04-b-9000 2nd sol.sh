82c86b344cdc3eb19b51a5b89d1e431e912f2296
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-24 22:50:30 +0300

    Update 04-b-9000 2nd sol.sh

--- FMI Tasks/Shell/04-b-9000 2nd sol.sh
+++ FMI Tasks/Shell/04-b-9000 2nd sol.sh
@@ -6,18 +6,16 @@ PROCESS_FILE="$(mktemp)"
 
 ps -e -o pid=,ppid= > "${PROCESS_FILE}"
 
-function get_children {
+function get_children() {
 	cat "${PROCESS_FILE}" | awk -v pid="{1}" '$2 == pid { print $1 }'
 }
 
-function count_children {
+function count_children() {
 	get_children "${1}" | wc -l
 }
 
-while read -d $'\n' LINE; do
-	read -d ',' pid ppid < <(echo "${LINE}")
-	[ $(count_children "${pid}") -ge $(count_children "${ppid}") ] &&\
-		echo "${pid}"
+while read PID PPID; do
+	[ $(count_children "${PID}") -ge $(count_children "${PPID}") ] && echo "${PID}"
 done < "${PROCESS_FILE}"
 
-rm "${PROCESS_FILE}"
+rm -- "${PROCESS_FILE}"
