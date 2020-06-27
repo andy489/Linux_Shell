277fc96756f842a7aab0445d6b2d4b505b1bd380
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-27 18:16:12 +0300

    Update 13 (solution 1).sh

--- FMI Tasks/Test 2 Preparation/13 (solution 1).sh
+++ FMI Tasks/Test 2 Preparation/13 (solution 1).sh
@@ -2,10 +2,16 @@
 # 13.sh solution 1
 # github.com/andy489
   
-[ $# -eq 1 ] || { echo "Invalid number of arguments. Usage: ${0} <dirname>"; exit 1; }
+[ $# -eq 1 ] || { 
+  echo "Invalid number of arguments. Usage: ${0} <dirname>" > &2
+  exit 1
+}
 
-d="${1}"
+D="${1}"
 
-[ -d "${d}" -a -r "${d}" ] || { echo "Not directory or not readable"; exit 2; }
+[ -d "${D}" -a -r "${D}" ] || { 
+  echo "Not directory or not readable." > &2
+  exit 2 
+}
 
-find "${d}" -type l -printf "%Y %p\n" 2>/dev/null | grep -e "^[^NL]" | cut -c3-
+find "${d}" -type l -printf "%Y %p\n" 2>/dev/null | grep -e "^[NL]" | cut -c3-
