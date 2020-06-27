bb47e77b20dcadb0023187637e959d816b4f7fc6
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-27 12:38:56 +0300

    Update 28 (A) cycle.sh

--- FMI Tasks/Test 2 Preparation/28 (A) cycle.sh
+++ FMI Tasks/Test 2 Preparation/28 (A) cycle.sh
@@ -2,30 +2,30 @@
 # 28.sh A) cycle
 # github.com/andy489
 
-temp=$(mktemp)
+TEMP=$(mktemp)
 
-cat | egrep "^[-+]?[0-9]+$" | sort -n > "${temp}"
+cat | egrep "^[-+]?[0-9]+$" | sort -n > "${TEMP}"
 
-[ "$(cat "$temp" | wc -l )" -ne 0 ] || { echo -n  "\nno valid numbers"; exit 1; } 
+[ "$(awk 'END{print NR}' "${TEMP}")" -ne 0 ] || { echo -n "\nno valid numbers"; exit 1; } 
 
-MAX="$(cat "$temp" | tail -n 1)"
-MIN="$(cat "$temp" | head -n 1)"
+MAX="$(tail -1 "${TEMP}")"
+MIN="$(head -1 "${TEMP}")"
 
-if [ $MAX -eq $MIN ];then
-    echo -e "\n$BIGGEST"
+if [ "${MAX}" -eq "${MIN}" ];then
+    echo -e "\n${MAX}"
 else
-    ABS_MIN="$( echo $MIN | sed -E "s/-//")"
-    ABS_MAX="$( echo $MAX | sed -E "s/-//")"
-    if [ $ABS_MIN -eq $ABS_MAX ];then
-        echo -e "\n$MIN"
-        echo "$MAX"
-    elif [ $ABS_MIN -lt $ABS_MAX ]; then
-        echo -e "$MAX"
+    ABS_MIN="$(sed -E "s/-//" <(echo "${MIN}"))"
+    ABS_MAX="$(sed -E "s/-//" <(echo "${MAX}"))"
+    if [ "${ABS_MIN}" -eq "${ABS_MAX}" ];then
+        echo -e "\n${MIN}"
+        echo "${MAX}"
+    elif [ "${ABS_MIN}" -lt "${ABS_MAX}" ]; then
+        echo -e "${MAX}"
     else
-        echo -e "\n$MIN"
+        echo -e "\n${MIN}"
     fi
 fi
 
-rm -- "${temp}"
+rm -- "${TEMP}"
 
 exit 0
