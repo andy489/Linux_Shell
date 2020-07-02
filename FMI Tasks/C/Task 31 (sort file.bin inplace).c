51f3ae629effaf86929a88094b622870fa1c0f7f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-02 18:27:48 +0300

    Update Task 31 (sort file.bin inplace).c

--- FMI Tasks/C/Task 31 (sort file.bin inplace).c
+++ FMI Tasks/C/Task 31 (sort file.bin inplace).c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <err.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <fcntl.h>
-#include <stdio.h>
+#include <sys/types.h>
 #include <stdint.h>
 #include <errno.h>
 #include <unistd.h>
@@ -17,63 +16,72 @@ int main(int argc, char **argv){
 
 	struct stat st;
 
-	if(stat(argv[1], &st) == -1){
-		err(2, "error while stat %s", file);
-	}	
+	if(stat(file, &st) == -1){
+		err(2, "could not stat file %s", file);
+	}
 
 	if(!(st.st_mode & S_IRUSR) || !(st.st_mode & S_IWUSR)){
-		errx(3, "file %s not readable or writable", file);
-	}	
+		errx(3, "file %s not readable or not writable", file);
+	}
+
+	/*
+	if(st.st_size > UINT64_MAX){
+		warnx("small chance of overfloating");
+	}
+	*/
+
+	uint64_t counting[256] = { 0 };
+	uint16_t level[256];
+	for (uint16_t j = 0; j< 256; ++j){
+		level[j] = 1;
+	}
 
 	ssize_t fd = open(file, O_RDWR);
 	if(fd == -1){
 		err(4, "error while opening file %s", file);
 	}
+
+	ssize_t read_sz;
+	uint8_t b;	
 	
-	//printf("size of binary file: %lld\n", st.st_size);
-	//printf("MAX size of uint32_t: %u\n", UINT32_MAX);
-
-	if(st.st_size > UINT32_MAX){
-		warnx("Counting sort might not work. Small chance of overflow");
-	}
-
-	uint32_t counting[256] = { 0 };
-
-	uint8_t b;
-	ssize_t read_sz;	
-	
-	while((read_sz = read(fd, &b, sizeof(b))) == sizeof(b)){
+	while((read_sz = read(fd, &b, sizeof(b))) == sizeof(b)){ 
 		++counting[b];
-	}
+		if(counting[b] == UINT16_MAX){
+			counting[b] = 0;
+			++level[b];
+		}
+	}	
 	if(read_sz == -1){
 		const int olderrno = errno;
-		close(fd);	
+		close(fd);
 		errno = olderrno;
 		err(5, "error while reading file %s", file);
 	}
-	
-	ssize_t ls = lseek(fd, 0, SEEK_SET);
-	if(ls == -1){
+
+	if(lseek(fd, 0, SEEK_SET) == -1){
 		const int olderrno = errno;
-		close(fd);	
+		close(fd);
 		errno = olderrno;
 		err(6, "error while lseek file %s", file);
 	}
 
 	uint16_t i = 0;
+
 	for(; i < 256; ++i){
-		b = i;
-		while(counting[i]){
-			if( write(fd, &b, sizeof(b)) != sizeof(b) ){
-				const int olderrno = errno;
-				close(fd);	
-				errno = olderrno;
-				err(7, "error while rewriting file %s", file);
+		while(level[i]--){
+			b = i;
+			while(counting[i]--){
+				if(write(fd, &b, sizeof(b)) != sizeof(b)){
+					const int olderrno = errno;
+					close(fd);
+					errno = olderrno;
+					err(7, "error while writing into file %s", file);
+				}
 			}
-			--counting[i];
+			counting[i] = UINT64_MAX;
 		}
 	}
-	
+
 	close(fd);
 	exit(0);
 }
