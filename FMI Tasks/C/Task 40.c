88cd63e09ba289f9e92625629918fc2874a6f033
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-06 23:16:34 +0300

    Update Task 40.c

--- FMI Tasks/C/Task 40.c
+++ FMI Tasks/C/Task 40.c
@@ -1,97 +1,89 @@
 // github.com/andy489
 
 #include <stdlib.h>
+#include <sys/types.h>
 #include <err.h>
-#include <errno.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-uint8_t isdig(const char c){
-	if(c>='0' && c<='9') return 1;
-	return 0;
+void usage(const char *executable){
+	errx(1, "Usage: %s [-c n | -c n-m | -d t -f n | -d t -f n-m]", executable);
 }
 
-void usage() {
-	errx(1, "Usage: <this executable> (-c n|-c n-m|-d c -f n|-d c -f n-m)");
+bool isdig(const char c){
+	if(c>='0' && c<='9')
+		return true;
+	return false;
 }
 
-int parse_digit(char symbol) {
-	if (!isdig(symbol)) usage();
+ssize_t parse_digit(char symbol){
+	if(!isdig(symbol)) 
+		usage("this executable");
 	return symbol - '0';
 }
 
-int parse_number(char** string) {
-	int n = 0;
-	for (; isdig((*string)[0]); (*string)++) {
-		n = n * 10 + parse_digit((*string)[0]);
-	}
+ssize_t parse_number(char **str){
+	ssize_t n = 0;
+	for(; isdig((*str)[0]); ++(*str))
+		n = n * 10 + parse_digit((*str)[0]);
 	return n;
 }
 
-void parse_range(char* arg, int* from, int* to) {
-	// arg is either 'A-B' or 'A'
-	if (!isdig(arg[0]) && arg[0] != '-') {
-		usage();
-	}
+void parse_range(char *arg, ssize_t *from, ssize_t *to){
+	// arg is either 'n' or 'n-m'
+	if(!isdig(arg[0]) && arg[0] != '-')
+		usage("this executable");
 
 	*from = parse_number(&arg);
-	if (arg[0] == '\0') {
+	if(arg[0] == '\0')
 		*to = *from;
-	} else if (arg[0] == '-') {
-		arg++;
-		if (arg[0] == '\0') {
-			*to = 1 << 15;
-		} else {
+	else if(arg[0] == '-'){ 
+		++arg;
+		if(arg[0] == '\0')
+			*to = 1<<15;
+		else
 			*to = parse_number(&arg);
-		}
 
-		if (arg[0] != '\0') {
-			usage();
-		}
-	} else {
-		usage();
-	}
+		if(arg[0] != '\0')
+			usage("this executable");
+	} else 
+		usage("this executable");
 }
 
-void cut(int start, int end, bool use_delim, char delim) {
+void cut(ssize_t start, ssize_t end, bool use_delim, char delim){
 	char ch;
-	int cnt = 0;
+	ssize_t cnt = 0, read_sz;
 
-	while(read(0, &ch, sizeof(ch)) > 0){
-		if ((!use_delim) || (ch == delim) || (cnt == 0)) {
+	while((read_sz = read(0, &ch, sizeof(ch))) == 1){
+		if((!use_delim) || (ch == delim) || (cnt == 0))
 			++cnt;
-		}
 
-		if (ch == '\n') {
+		if(ch == '\n'){
 			write(1, "\n", 1);
 			cnt = 0;
-		} else if (cnt >= start && cnt <= end) {
-			if (!((cnt == start) && (ch == delim) && use_delim)) {
-			    write(1, &ch, sizeof(ch));
-			}
+		} else if(cnt >= start && cnt <= end){
+			if(!((cnt == start) && (ch == delim) && use_delim))
+				write(1, &ch, 1);
 		}
-	} 
-	
+	}
+	if(read_sz == -1)
+		err(2, "could not read from stdin");
 }
 
-int main(int argc, char **argv){
-	int     start = -1, end = -1;
-	char    delim = '\0';
 
-	if ((argc == 3) && (strcmp(argv[1], "-c") == 0)) {
+int main(int argc, char **argv){
+	ssize_t start = -1, end = -1;
+	char delim = '\0';
+	if((argc == 3) && (strcmp(argv[1], "-c") == 0)){
 		parse_range(argv[2], &start, &end);
 		cut(start, end, false, delim);
-	} else if ((argc == 5) && (strcmp(argv[1], "-d") == 0) && (strcmp(argv[3], "-f") == 0)){
+	} else if((argc == 5) && (strcmp(argv[1], "-d") == 0)){
 		parse_range(argv[4], &start, &end);
 		delim = argv[2][0];
 		cut(start, end, true, delim);
-	} else {
-		usage();
-	}
+	} else
+		usage(argv[0]);
 	exit(0);
 }
