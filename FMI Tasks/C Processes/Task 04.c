765ff476f6efcd8e05e7dbc38d62670d7727c987
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 12:25:20 +0300

    Update Task 04.c

--- FMI Tasks/C Processes/Task 04.c
+++ FMI Tasks/C Processes/Task 04.c
@@ -4,19 +4,16 @@
 
 // github.com/andy489
 #include <stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #define N 100000
 
 int main(){
-	if(fork() > 0){
-		for(int i=0; i<N; ++i){
+	if(fork() > 0)
+		for(int i=0; i<N; ++i)
 			printf("parent\n");
-		}
-	} else {
-		for(int i=0; i<N; ++i){
+	else 
+		for(int i=0; i<N; ++i)
 			printf("child\n");
-		}
-	}
 	exit(0);
 }
