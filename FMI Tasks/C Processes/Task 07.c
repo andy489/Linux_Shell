e1ac27af1b7dfa9c7186131a9123ab76bfe6b473
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 13:58:46 +0300

    Update Task 07.c

--- FMI Tasks/C Processes/Task 07.c
+++ FMI Tasks/C Processes/Task 07.c
@@ -7,83 +7,70 @@
 // github.com/andy489
 #include <stdlib.h>
 #include <err.h>
-#include <errno.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
-	if(argc!=4){
-		errx(1,"Invalid number of arguments. Usage %s <cmd1> <cmd2> <cmd3>", argv[0]);
-	}
+	if(argc != 4)
+		errx(1, "Invalid number of arguments. Usage: %s <cmd1> <cmd2> <cmd3>", argv[0]);
 
 	const char *cmd1 = argv[1], *cmd2 = argv[2], *cmd3 = argv[3];
 
-	const pid_t child1 = fork();
-	if(child1 == -1){
-		err(2,"could not fork first process");
-	}
+	pid_t cmd1_pid = fork();
+	if(cmd1_pid == -1)
+		err(2, "could not fork for %s", cmd1);
 
-	if(child1 == 0){
-	// we are in first child
-		if(execlp(cmd1, "pesho_1", (char *)NULL) == -1){
-			err(3,"failed to execlp %s", cmd1);
-		}
-	}
+	if(cmd1_pid == 0) // we are in child for cmd1
+		if(execlp(cmd1, cmd1, (char*)NULL) == -1)
+			err(3, "could not execlp %s", cmd1);
 
-	int status1;
-	const pid_t wait_code1 = wait(&status1);
-	if(wait_code1 == -1) {
-		err(4,"could not wait first child");
-	}
+	int stat_cmd1;
+	if(wait(&stat_cmd1) == -1)
+		err(4, "could not wait for %s", cmd1);
 
-	if(WIFEXITED(status1)){
-		printf("first child, pid: %d, exit code: %d\n", child1, WEXITSTATUS(status1));
-	}
+	if(!WIFEXITED(stat_cmd1))
+		errx(5, "%s did not terminated normally", cmd1);
 
-	const pid_t child2 = fork();
-	if(child2 == -1){
-		err(5,"could not fork second process");
-	}
+	printf("%s, pid: %d, exit code: %d\n", cmd1, cmd1_pid, WEXITSTATUS(stat_cmd1));
 	
-	if(child2 == 0){
-	// we are in second child
-		if(execlp(cmd2, "pesho_2", (char *)NULL) == -1){
-			err(6,"failed to execlp %s", cmd2);
-		}
-	}
+	// ---- ---- ---- cmd1
 
-	int status2;
-	const pid_t wait_code2 = wait(&status2);
-	if(wait_code2 == -1){
-		err(7,"could not wait secont process");
-	}
+	pid_t cmd2_pid = fork();
+	if(cmd2_pid == -1)
+		err(6, "could not fork for %s", cmd2);
 
-	if(WIFEXITED(status2)){	
-		printf("second child, pid: %d, exit code: %d\n", child2, WEXITSTATUS(status2));
-	}
+	if(cmd2_pid == 0) // we are in child for cmd2
+		if(execlp(cmd2, cmd2, (char*)NULL) == -1)
+			err(7, "could not execlp %s", cmd2);
 
-	const pid_t child3 = fork();
-	if(child3 == -1){
-		err(8,"could not fork third process");
-	}
+	int stat_cmd2;
+	if(wait(&stat_cmd2) == -1)
+		err(8, "could not wait for %s", cmd2);
 
-	if(child3 == 0){
-	// we are in third child
-		if(execlp(cmd3, "pesho_3", (char *)NULL) == -1){
-			err(9,"failed to execlp %s", cmd3);
-		}
-	}
+	if(!WIFEXITED(stat_cmd2))
+		errx(9, "%s did not terminated normally", cmd2);
+	
+	printf("%s, pid: %d, exit code: %d\n", cmd2, cmd2_pid, WEXITSTATUS(stat_cmd2));
+	
+	// ---- ---- ---- cmd2
 
-	int status3;
-	const pid_t wait_code3 = wait(&status3);
-	if(wait_code3 == -1){
-		err(10,"could not wait thord child");
-	}
+	pid_t cmd3_pid = fork();
+	if(cmd3_pid == -1)
+		err(10, "could not fork for %s", cmd3);
 
-	if(WIFEXITED(status1)){
-		printf("third child, pid: %d, exit code: %d\n", child3, WEXITSTATUS(status3));
-	}
+	if(cmd3_pid == 0) // we are in child for cmd3
+		if(execlp(cmd3, cmd3, (char*)NULL) == -1)
+			err(11, "could not execlp %s", cmd3);
 
-	printf("finished\n");
+	int stat_cmd3;
+	if(wait(&stat_cmd3) == -1)
+		err(12, "could not wait for %s", cmd3);
+
+	if(!WIFEXITED(stat_cmd3))
+		errx(13, "%s did not terminated normally", cmd3);
+
+	printf("%s, pid: %d, exit code: %d\n", cmd3, cmd3_pid, WEXITSTATUS(stat_cmd3));
+	
 	exit(0);
 }
