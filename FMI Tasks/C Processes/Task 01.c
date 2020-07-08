66afa5cf2c9bf08d47990679b29379012c312449
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 12:13:14 +0300

    Update Task 01.c

--- FMI Tasks/C Processes/Task 01.c
+++ FMI Tasks/C Processes/Task 01.c
@@ -3,18 +3,14 @@
 */
 
 // github.com/andy489
-#include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
-#include <errno.h>
 #include <stdio.h>
 
 int main(){
 	const char * cmd = "date";
-	if(execl("/bin/date", "date", "+%D", (char *)NULL) == -1){
+	if(execl("/bin/date", "date", "+%D", (char *)NULL) == -1)
 		err(1,"failed to execl %s", cmd);
-	} else {
-		printf("this will never be printed\n");		
-	}		
-	exit(0);
+	else 
+		printf("this will never be printed\n");				
 }
