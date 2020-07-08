8b3b190aadbec3ccf7c3f7e9bef847d84f0bb303
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 21:51:10 +0300

    Update Task P02.c

--- FMI Tasks/C Processes/Task P02.c
+++ FMI Tasks/C Processes/Task P02.c
@@ -11,43 +11,38 @@
 #include <string.h>
 
 int main(int argc, char **argv){
-	if(argc!=2){
+	if(argc != 2)
 		errx(1, "Invalid number of arguments. Usage %s <string>", argv[0]);
-	}
 
 	const char *str = argv[1];
-
-	int pf[2];
-	if(pipe(pf) == -1){
-		errx(2, "could not create pipe");
-	}
+	ssize_t len = (ssize_t)strlen(str);
+	
+	int p[2];
+	if(pipe(p) == -1)
+		err(2, "could not create pipe");
 
 	const pid_t child_pid = fork();
-	if(child_pid == -1){
+	if(child_pid == -1)
 		err(3, "could not fork child process");
-	}
 
-	if(child_pid == 0){
-		// we are in child process
-		close(pf[1]);
-		char buf;
-		ssize_t rd_size = -1;
-		while((rd_size = read(pf[0], &buf, 1)) > 0){
-			write(1, &buf, 1);
-		}
-		if(rd_size == -1){
-			err(4,"error while reading in child process");
-		}
-		close(pf[0]);
+	if(child_pid == 0){ // we are in child process
+		close(p[1]);
+		
+		if(read(p[0], &str, len) == len){
+			if(write(1, &str, len) != len)
+				err(4, "could not write to stdout");
+		} else
+			err(5,"error while reading in child process");
+			
+		close(p[0]);
 		exit(0);
 	}
 	
-	close(pf[0]);
-	ssize_t len = (ssize_t)strlen(str);
-	if(write(pf[1], str, len) != len){
-		err(4, "error while writing in parent");
-	}
-	close(pf[1]);
+	close(p[0]);
+	if(write(p[1], str, len) != len)
+		err(6, "error while writing in parent");
+	
+	close(p[1]);
 	wait(NULL);
 	exit(0);
 }
