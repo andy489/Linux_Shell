79014a48faae183cedc21d88869717ba874de8ee
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 13:01:27 +0300

    Update Task 06.c

--- FMI Tasks/C Processes/Task 06.c
+++ FMI Tasks/C Processes/Task 06.c
@@ -5,45 +5,34 @@
 
 // github.com/andy489
 #include <stdlib.h>
-#include <err.h>
-#include <errno.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <err.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
-	if(argc!=2){
-		errx(1,"Invalid number of arguments. Usage: %s <cmd>", argv[0]);
-	}
+	if(argc != 2)
+		err(1, "Invalid number of arguments. Usage: %s <cmd>", argv[0]);
 
 	const char *cmd = argv[1];
-	
-	const pid_t child_pid = fork();
-	if(child_pid == -1){
-		err(2, "failed to fork");
-	}
-	
-	if(child_pid == 0){ // we are in child process
-		if(execlp(cmd, "pesho_no_drama", (char *)NULL) == -1){
-			err(3,"failed to execlp %s", cmd);
-		}
-	}
-	
-	int child_status;
-	const pid_t wait_code = wait(&child_status);
 
-	if(wait_code == -1){
-		err(4,"could not wait for child");
-	}
-
-	if(!WIFEXITED(child_status)){
-		err(5,"child did not terminate normally");
-	}
-
-	if(WEXITSTATUS(child_status)){
-		err(6,"child exit code not 0");
-	}
+	pid_t child_pid = fork();
+	if(child_pid == -1)
+		err(2, "could not fork");
+	if(child_pid == 0) // we are in child process
+		if(execlp(cmd, "drama lama", (char*)NULL) == -1)
+			err(3, "could not execlp %s", cmd);
 	
+	int child_exit_status;
+	if(wait(&child_exit_status) == -1)
+		err(4, "could not wait for child");
+
+	if(!(WIFEXITED(child_exit_status)))
+		errx(5, "child did not terminate normally");
+
+	if(WEXITSTATUS(child_exit_status))
+		errx(6, "child exit code not 0");
+
 	printf("command %s was successfully executed\n", cmd);
 
 	exit(0);
