118ee3b387e73fd8ca7a127e437afb1ac977c90b
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-08 14:37:18 +0300

    Update Task 08.c

--- FMI Tasks/C Processes/Task 08.c
+++ FMI Tasks/C Processes/Task 08.c
@@ -16,25 +16,21 @@
 #include <string.h>
 
 int main(int argc, char **argv){
-	if(argc!=2){
+	if(argc!=2)
 		errx(1, "Invalid number of arguments. Usage: %s <filename>", argv[0]);
-	}
 
 	const char *file = argv[1];
 	
-	ssize_t fd = open(file, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP);
-	if(fd == -1){
+	ssize_t fd = open(file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
+	if(fd == -1)
 		err(2, "could not open file %s", file);
-	}
 
 	const pid_t child_pid = fork();
-	if(child_pid == -1){
+	if(child_pid == -1)
 		err(3, "could not fork process");
-	}
 
-	if(child_pid == 0){
-		// we are in child process		
-		const char *text = "foobar\n";
+	if(child_pid == 0){ // we are in child process		
+		const char *text = "foobar";
 		const ssize_t len = strlen(text);		
 
 		if(write(fd, text, len) != len){
@@ -65,9 +61,8 @@ int main(int argc, char **argv){
 		err(6, "child did not terminate normally");
 	}
 
-	if(WEXITSTATUS(status) != 0){
+	if(WEXITSTATUS(status) != 0)
 		err(7, "child exit code not 0, fd should be closed");
-	}
 	
 	fd = open(file, O_RDONLY);
 	if(fd == -1){
