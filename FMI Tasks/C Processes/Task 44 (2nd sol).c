6f35489197818496d1adb803c84e6a3af1ac04aa
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-09 20:07:59 +0300

    Update Task 44 (2nd sol).c

--- FMI Tasks/C Processes/Task 44 (2nd sol).c
+++ FMI Tasks/C Processes/Task 44 (2nd sol).c
@@ -1,6 +1,6 @@
 // github.com/andy489
-// github.com/andy489
 // 2nd solution
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
@@ -8,41 +8,41 @@
 #include <sys/wait.h>
 
 int main(void){
-    char buf[1<<6];
-    ssize_t read_sz;
+    	char buf[1<<6];
+    	ssize_t read_sz;
+	
+	const char *prompt = "Stamat's simple prompt: ";
+	const ssize_t len = (ssize_t)strlen(prompt);
 
-	 const char *prompt = "Stamat's simple prompt: ";
-	 const ssize_t len = (ssize_t)strlen(prompt);
-
-    if(write(1, prompt, len) != len)
+    	if(write(1, prompt, len) != len)
 		err(1, "could not write prompt to stdout");
 
-    while((read_sz = read(0, buf, sizeof(buf))) > 0){
-        char cmd[1<<6];
-        ssize_t i = 0;
-        for(; i < read_sz; ++i){
-            if(buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t')
-                break;
-            cmd[i] = buf[i];
+    	while((read_sz = read(0, buf, sizeof(buf))) > 0){
+        	char cmd[1<<6];
+        	ssize_t i = 0;
+        	for(; i < read_sz; ++i){
+            		if(buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t')
+                		break;
+           	cmd[i] = buf[i];
         }
         cmd[i]='\0';
 
         if(strlen(cmd) == 0 || strcmp(cmd, "exit") == 0)
-            break;
+		break;
 
         const pid_t child = fork();
         if(child == -1)
-            err(2, "could not fork");
+		err(2, "could not fork");
 
-        if(child == 0){ // we are in child process for exec cmd
-            if(execlp(cmd, cmd, (char*)NULL) == -1)
-                err(3, "could not execlp: %s", cmd);
-        }
+        if(child == 0) // we are in child process for exec cmd
+            	if(execlp(cmd, cmd, (char*)NULL) == -1)
+                	err(3, "could not execlp: %s", cmd);
 
         if(wait(NULL) == -1)
-            err(4, "could not wait");
+		err(4, "could not wait");
+	    
         if(write(1, prompt, len) != len)
-		      err(5, "could not write ptompt to stdout");
+		err(5, "could not write ptompt to stdout");
     }
     exit(0);
 }
