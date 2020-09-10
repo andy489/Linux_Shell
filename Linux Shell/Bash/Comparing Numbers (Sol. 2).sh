8d2781791cbd9800aab27b18985289d5c3bfc2c5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-09-10 11:22:17 +0300

    Create Comparing Numbers (Sol. 2).sh

---
+++ Linux Shell/Bash/Comparing Numbers (Sol. 2).sh
@@ -0,0 +1,13 @@
+# github.com/andy489
+
+#!/bin/bash
+
+read X
+read Y
+if [ "$X" -gt "$Y" ]; then
+echo "X is greater than Y"
+elif [ "$X" -lt "$Y" ]; then
+echo "X is less than Y"
+elif [ "$X" -eq "$Y" ]; then
+echo "X is equal to Y"
+fi
