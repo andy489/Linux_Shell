309ea153734ef826c1a180ec27986efe5633f562
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-09-11 10:56:45 +0300

    Update 05-b-4200 3rd sol.sh

--- FMI Tasks/Shell/05-b-4200 3rd sol.sh
+++ FMI Tasks/Shell/05-b-4200 3rd sol.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://github.com/steneva
+
+# we assume that there is only one curly bracket per row
+
 awk 'BEGIN{level=0;maxLevel=0;}\
         /{/{level++}\
         /}/{level--}\
